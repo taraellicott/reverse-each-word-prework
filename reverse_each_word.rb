40195dbd863f5d27fda1e52c7455fc7661474d5a
Tara Ellicott <taraellicott@gmail.com>
2018-04-15 20:58:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
-end
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|word| word.reverse}.join(" ")
+# end
 
 def reverse_each_word(sentence)
   sentence.split.each do |word| 
   word.reverse
 end
-word.join(" ")
+sentence.join(" ")
 end
