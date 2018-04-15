52efc005401e76c8a100960260e5ada2f727acc7
Tara Ellicott <taraellicott@gmail.com>
2018-04-15 20:56:06 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,35 @@
+# def reverse_each_word(sentence)
+# reversed = []
+# sentence.split.each do |word|
+# reversed.unshift word
+# end
+# reversed.join(' ')
+# end
+
+# def reverse_word_order(sentence)
+#   reversed = []
+#   sentence.split.each do |word|
+#     reversed.unshift word
+#   end
+#   reversed.join(' ')
+# end
+
+# def change_nums(nums)
+#   new_count = []
+#   nums.each do |x|
+#     x += 1
+#     new_count << x
+#   end
+#   new_count
+# end
+
+def reverse_each_word(sentence)
+  sentence.split.each do |word| 
+   
+end
+ word.reverse.join(" ")
+end
+
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|word| word.reverse}.join(" ")
+# end
